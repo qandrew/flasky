63ab453732a70150339df094fac09a01d74f818a
Andrew Xia <axia@mit.edu>
2020-01-02 19:33:13 -0800

    ch2 templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
+
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
---
+++ init.sh
@@ -0,0 +1,3 @@
+
+source venv/bin/activate
+export FLASK_APP=microblog.py

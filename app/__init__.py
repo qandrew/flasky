d05c63d26734f4f8f87200bb138b108794b1f117
Andrew Xia <axia@mit.edu>
2020-04-24 09:39:30 -0700

    ch5

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,14 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.secret_key = "super secret key" # TODO: delete? not in tutorial
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models

c351939cff2e6d83e60dde266ea49d6aafc5dbde
Andrew Xia <axia@mit.edu>
2020-04-26 17:05:25 -0700

    extract

--- app/routes.py
+++ app/routes.py
@@ -13,7 +13,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
-    g.locale = str(get_locale())
+    # g.locale = str(get_locale())
 
 
 @app.route('/', methods=['GET', 'POST'])

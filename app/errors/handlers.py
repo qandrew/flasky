a90fbfc4eb9578a6a695e54b2d9a56329d727666
Andrew Xia <axia@mit.edu>
2020-04-27 19:33:11 -0700

    ch15 massive refactor

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,11 +1,12 @@
 from flask import render_template
-from app import app, db
+from app.errors import bp
+from app import db
 
-@app.errorhandler(404)
+@bp.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
 
-@app.errorhandler(500)
+@bp.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), 500
--- app/translate.py
+++ app/translate.py
@@ -1,13 +1,13 @@
 import json
 import requests
 from flask_babel import _
-from app import app
+from app import current_app
 
 def translate(text, source_language, dest_language):
-    # if 'MS_TRANSLATOR_KEY' not in app.config or \
+    # if 'MS_TRANSLATOR_KEY' not in current_app.config or \
     #         not app.config['MS_TRANSLATOR_KEY']:
     #     return _('Error: the translation service is not configured.')
-    # auth = {'Ocp-Apim-Subscription-Key': app.config['MS_TRANSLATOR_KEY']}
+    # auth = {'Ocp-Apim-Subscription-Key': current_app.config['MS_TRANSLATOR_KEY']}
     # r = requests.get('https://api.microsofttranslator.com/v2/Ajax.svc'
     #                  '/Translate?text={}&from={}&to={}'.format(
     #                      text, source_language, dest_language),
